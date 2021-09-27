5a24dd2f94d0ea704d9560c9ed3472c082669034
lost-hope <s.willrodt@yahoo.de>
2021-09-27 15:11:08 +0200

    Added Play command

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,6 @@
 module.exports = {
 	name: 'ping',
+	aliases: [],
 	description: 'this is a ping command!',
 	execute(message, args, cmd, client, Discord) {
 		message.channel.send('pong!');
--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const { Client, Intents, Collection } = require('discord.js');
 const Discord = require('discord.js');
 require('dotenv').config();
 
-const client = new Client({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
+const client = new Client({
+	intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES ]
+});
 
 client.commands = new Collection();
 client.events = new Collection();
